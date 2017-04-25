bf59d6bc89c17fbd0e66a75d1ff3aa483ac5026f
Ashish Agrawal <ashishagrawal979@gmail.com>
2017-04-25 23:09:18 +0530

    Add authentication to the application
    This commit implements login, logout, sign up and reset password
    functionality. For this devise gem is used.

--- Gemfile
+++ Gemfile
@@ -33,7 +33,8 @@ gem 'bootstrap-sass', '~> 3.3'
 gem 'rails-assets-tether', '>= 1.1.0'
 # font awesome for icons
 gem 'font-awesome-rails', '~> 4.6.3'
-
+# for authentication
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module Erp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_mailer.default_url_options = { host: Rails.application.secrets.app_host }
+
+    config.action_mailer.smtp_settings = {
+        address: Rails.application.secrets.smtp_host,
+        port: Rails.application.secrets.smtp_port,
+        user_name: Rails.application.secrets.sender_email,
+        password: Rails.application.secrets.sender_password
+    }
   end
 end
